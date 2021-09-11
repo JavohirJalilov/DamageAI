4356057ee20c924df869b85d06a489ac9bf64ebb
Jalilov <javohirjalilov777@gmail.com>
2021-09-11 07:00:34 +0500

    drawing bbox image

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from image_inf import image_information
 file_name = 'image_0000592'
 
 img = cv2.imread(f"sample/images/{file_name}.jpg")
-img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+# img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-print(image_information(file_name))
+size,data_contours = image_information(file_name)
+
+for i in data_contours:
+	x_min,y_min,x_max,y_max,classes = i
+	img = cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(0,0,255),3)
+
+cv2.imwrite('result.jpg',img)
 plt.imshow(img)
 plt.show()
