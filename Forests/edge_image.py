83c30b5570ee7e79e96731acdb591bfc8183f544
Jalilov <javohirjalilov777@gmail.com>
2021-09-16 11:14:48 +0500

    result image

--- Forests/edge_image.py
+++ Forests/edge_image.py
@@ -7,7 +7,7 @@ def kernal(x,y):
   k = np.ones((x,y),dtype=np.uint8)
   return k
 
-filename = 'DeepCrack/train_img/11116-2.jpg'
+filename = 'DeepCrack/test_img/11142-2.jpg'
 
 parser = argparse.ArgumentParser()
 # parser.add_argument('-i', '--input', help='path to the input image', 
@@ -29,7 +29,8 @@ canny = cv2.Canny(blurred, 50, 200)
 edge_detector = cv2.ximgproc.createStructuredEdgeDetection('model.yml/model.yml')
 # detect the edges
 edges = edge_detector.detectEdges(image)
-add_img = np.hstack((canny,edges)) 
-
+# edges = cv2.cvtColor(edges,cv2.COLOR_GRAY2RGB)
+# add_img = np.hstack((orig_image[:,:,1],canny,edges)) 
+# cv2.imwrite('result.jpg',edges)
 plt.imshow(edges,cmap='gray')
 plt.show()
